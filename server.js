8eea13654126371c0cfcf5df0b20d6c2eed42c90
An Bui <anhipchipchip@gmail.com>
2022-10-13 23:09:08 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 const express = require("express")
 const app = express()
 const port = 3002
+const responses = [
+    "It is certain",
+ "It is decidedly so", 
+ "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+   "As I see it yes",
+    "Most likely",
+     "Outlook good",
+     "Yes", 
+     "Signs point to yes", 
+     "Reply hazy try again",
+      "Ask again later",
+      "Better not tell you now",
+       "Cannot predict now",
+        "Concentrate and ask again",
+        "Don't count on it",
+         "My reply is no", 
+         "My sources say no",
+         "Outlook not so good",
+          "Very doubtful"]
 
 app.get("/greeting", (req, res) => {
     res.send("Hello, stranger" )
@@ -11,4 +32,15 @@ app.get("/greeting", (req, res) => {
 app.get("/tip/:total/:tipPercentage", (req,res)=> {
     res.send(`${parseInt(req.params.total) * parseInt(req.params.tipPercentage)/100}`)
 })
+app.get("/magic/:question", (req,res) => {
+    res.send(`<h1>${req.params.question} = ${responses[Math.floor(Math.random() *responses.length)]}</h1>`)
+})
+
+
+
+
+
+
+
 app.listen(3002)
+
